5ce777b27a47e7d31bf717ad0e58d9a1b8dc33a5
di§8s <holper18@gmail.com>
2019-01-03 22:55:19 +0400

    ..

--- nod.playground/Contents.swift
+++ nod.playground/Contents.swift
@@ -1,10 +1,9 @@
 import UIKit
 
 var array = [2, 10, 12, 14, 21, 99, 67, 2882]
-let f = array.sorted()
 
 //алгоритм евклида для наибольшего общего делителя, можно было через остаток от деления
-func nod(a: Int, b: Int)-> Int{
+func nod(a: Int, b: Int) -> Int{
     var x = a
     var y = b
     while x != y{
@@ -17,7 +16,7 @@ func nod(a: Int, b: Int)-> Int{
     }
     return x
 }
-func nok(a: [Int])->Int{
+func nok(a: [Int]) -> Int{
     let filterArray = a.sorted()
     
     guard let i = filterArray.last else {fatalError("массив пустой")}
@@ -25,19 +24,22 @@ func nok(a: [Int])->Int{
     let x = i
     let y = filterArray[filterArray.count - 2]
     
-    let n = x*y/nod(a: x, b: y)
-    var localN = n
+    let nok_ = x * y / nod(a: x, b: y)
+    var localNok = nok_
     var t = 0
+    
+    // когда прошлись полностью по массиву надо пройтись еще раз чтобы проверить что нашли наименьшее общее кратное, как сделать проверку сколько раз проходить не придумал сделал 4 раза =)
     while t != 3 {
-        for element in filterArray{  // когда прошлись полностью по массиву надо пройтись еще раз
-            while localN % element != 0 {
-                localN += n
+        for element in filterArray{
+            while localNok % element != 0 {
+                localNok += nok_
             }
         }
         t += 1
     }
     
-    return localN
+    return localNok
 }
+
 nok(a: array)
 
